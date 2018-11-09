9a360b111972cd2658ec034eb5c0425ab8b01889
Jonathas F. Silva <jonathas_ssilva@hotmail.com>
2018-11-09 10:51:02 -0300

    batalhaNaval

--- naoConfiaBatalha.py
+++ naoConfiaBatalha.py
@@ -1,6 +1,36 @@
 """Juvenal nao confia em ninguem."""
 
 
+def exploraDiagPriAsc(l, c, campo, m, n):
+    """Explora diagonal primaria ascendente."""
+    if l+1 <= m-1 and c+1 <= n-1:
+        if campo[l+1][c+1] == '.':
+            pass
+        elif campo[l+1][c+1] == 't':
+            exploraDiagPriAsc(l+1, c+1, campo, m, n)
+        elif campo[l+1][c+1] == '#':
+            return False
+    elif m < l+1 or n < c+1:
+        pass
+    else:
+        return False
+
+
+def exploraDiagPriDes(l, c, campo, m, n):
+    """Explora diagonal primaria descendente."""
+    if m-1 <= l-1 and n-1 <= c-1:
+        if campo[l-1][c-1] == '.':
+            pass
+        elif campo[l-1][c-1] == 't':
+            exploraDiagPriDes(l-1, c-1, campo, m, n)
+        elif campo[l-1][c-1] == '#':
+            return False
+    elif l-1 < m or c-1 < n:
+        pass
+    else:
+        return False
+
+
 def explorarCima(l, c, campo):
     """Explora Cima."""
     if 0 <= l-1:
@@ -16,7 +46,7 @@ def explorarCima(l, c, campo):
         return False
 
 
-def explorarBaixo(l, c, campo, m):
+def explorarBaixo(l, c, m):
     """Explora em baixo."""
     if l+1 <= m-1:
         if campo[l+1][c] == '.':
@@ -67,9 +97,15 @@ def explorar(l, c, campo, m, n):
     baixo = explorarBaixo(l, c, campo, m)
     esquerda = explorarEsquerda(l, c, campo)
     direita = explorarDireita(l, c, campo, n)
+    diagPriAsc = exploraDiagPriAsc(l, c, campo, m, n)
+    diagPriDes = exploraDiagPriDes(l, c, campo, m, n)
 
-    if cima == baixo == esquerda == direita:
+    if cima == baixo == esquerda == direita == diagPriAsc == diagPriDes:
         return True
+
+def explorar2(l, c, campo, m, n):
+    explorar2(l-1, c, campo, m, n)
+
 
 
 def tiro(l, c, campo):
@@ -78,9 +114,7 @@ def tiro(l, c, campo):
         campo[l][c] = 't'
 
 
-tamanho = input().split()
-
-m, n = tamanho
+m, n = input().split()
 m, n = int(m), int(n)
 
 campo = []
